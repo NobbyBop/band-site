389cea8cd323da9033552536a1babff77bb5e93a
Nicholas Mirigliani <36427716+NobbyBop@users.noreply.github.com>
2024-12-02 13:44:52 -0500

    name change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/band-site/", // Replace <repo-name> with your repository name
+  base: "/mobiusline/", // Replace <repo-name> with your repository name
   plugins: [react()],
 });
