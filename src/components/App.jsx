e98b728da4c92cccfaca8ad9340fb94c88f7c875
Nicholas Mirigliani <36427716+NobbyBop@users.noreply.github.com>
2024-12-05 12:04:17 -0500

    home photos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <header>
-        <h1>MOBIUS LINE</h1>
+        <img src="/mobiusline/images/text_logo.png" className="header-logo" />
         <Navigation />
       </header>
       <Routes>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,17 @@
 import "./App.css";
 
 function Home() {
+  let photos = [
+    { id: 1, source: "/mobiusline/images/elevator.png" },
+    { id: 2, source: "/mobiusline/images/climb.png" },
+    { id: 3, source: "/mobiusline/images/lean.png" },
+    { id: 4, source: "/mobiusline/images/beatles.png" },
+    { id: 5, source: "/mobiusline/images/lie_down.png" },
+    { id: 6, source: "/mobiusline/images/babbio.png" },
+    { id: 7, source: "/mobiusline/images/duel.png" },
+    { id: 8, source: "/mobiusline/images/park_stacked.png" },
+    { id: 9, source: "/mobiusline/images/theatre.png" },
+  ];
   return (
     <>
       <h2>Home</h2>
@@ -8,6 +19,16 @@ function Home() {
         Mobius Line Band information here. Some pictures and social links as
         well.
       </p>
+
+      <h3>Photos</h3>
+      <div className="home-photos-container">
+        {photos.map((item) => (
+          <div className="home-photos" key={item.id}>
+            <img src={item.source} className="home-image" />
+            <p>Caption undefined for photo_id={item.id}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
